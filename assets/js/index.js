e2d4bda4f5634638b01fce431aecdf680fcaf2d9
shubham14p3 <shubham14p3@gmail.com>
2020-06-20 01:13:33 +0530

    add the logic against reclick of the board

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,191 +1,196 @@
 // using the object oriented way
 
-const boardUtils = (function() {
-    const WINNING_COMBINATIONS = [
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8],
-        [0, 4, 8],
-        [2, 4, 6],
+const boardUtils = (function () {
+  const WINNING_COMBINATIONS = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  return {
+    WINNING_COMBINATIONS,
+  };
+})();
 
-    ];
-    return {
-        WINNING_COMBINATIONS,
-    };
-}());
+const gameUtils = (function () {
+  const container = document.getElementById('container');
 
-const gameUtils = (function() {
-    const container = document.getElementById('container');
+  const winningMessage = (player) =>
+    winDrawUI(`Congratulation ${player} is the winner!!`);
 
-    const winningMessage = (player) => winDrawUI(`Congratulation ${player} is the winner!!`);
+  const drawMessage = () => winDrawUI("It's a draw ");
 
-    const drawMessage = () => winDrawUI("It's a draw ");
+  const handleGameReset = () => {
+    return location.reload();
+  };
 
-    const handleGameReset = () => {
-        return location.reload()
-    }
+  const winDrawUI = (message) => {
+    // container.innerHTML = '';
+    const div = document.createElement('div');
+    div.setAttribute('class', 'bg-dark');
 
-    const winDrawUI = (message) => {
-        // container.innerHTML = '';
-        const div = document.createElement('div');
-        div.setAttribute('class', 'bg-dark')
+    // banner
+    const h1 = document.createElement('h1');
+    h1.innerHTML = `Tic-Tac-Toe`;
+    h1.setAttribute('class', 'banner');
+    div.appendChild(h1);
 
-        // banner
-        const h1 = document.createElement('h1');
-        h1.innerHTML = `Tic-Tac-Toe`;
-        h1.setAttribute('class', 'banner');
-        div.appendChild(h1);
+    // the congratulation message
+    const h2 = document.createElement('h2');
+    h2.innerHTML = message;
+    h2.setAttribute('class', 'winner');
+    div.appendChild(h2);
 
-        // the congratulation message
-        const h2 = document.createElement('h2');
-        h2.innerHTML = message
-        h2.setAttribute('class', 'winner');
-        div.appendChild(h2);
+    // reset the game
+    const btn = document.createElement('btn');
+    btn.setAttribute('class', 'btn btn-warning reset-btn');
+    btn.innerHTML = 'Reset';
+    btn.addEventListener('click', () => handleGameReset());
+    div.appendChild(btn);
 
-        // reset the game
-        const btn = document.createElement('btn');
-        btn.setAttribute('class', 'btn btn-warning reset-btn')
-        btn.innerHTML = 'Reset'
-        btn.addEventListener('click', () => handleGameReset())
-        div.appendChild(btn);
+    // restart the game
+    const btnRestart = document.createElement('btn');
+    btnRestart.setAttribute('class', 'btn btn-success reset-btn');
+    btnRestart.innerHTML = 'Restart the Game';
+    btnRestart.addEventListener('click', () => handleGameRestart(div));
+    div.appendChild(btnRestart);
 
-        // restart the game
-        const btnRestart = document.createElement('btn');
-        btnRestart.setAttribute('class', 'btn btn-success reset-btn')
-        btnRestart.innerHTML = 'Restart the Game'
-        btnRestart.addEventListener('click', () => handleGameRestart(div))
-        div.appendChild(btnRestart);
+    container.appendChild(div);
+    // container.setAttribute('class', 'bg-dark');
+    return container;
+  };
+  const handleGameRestart = (node) => {
+    node.style.display = 'none';
+    board.reset();
+    return (counter = 0);
+  };
 
-        container.appendChild(div);
-        // container.setAttribute('class', 'bg-dark');
-        return container;
-    };
-    const handleGameRestart = (node) => {
-        node.style.display = 'none'
-        board.reset();
-        return counter = 0;
-    }
-
-
-    return {
-        winningMessage,
-        drawMessage,
-    };
-}());
+  return {
+    winningMessage,
+    drawMessage,
+  };
+})();
 
 const game = new TicTacToe();
 
 game.start();
 
 function TicTacToe() {
-    // body...
-    const board = new Board();
-    const firstPlayer = new Player('EXES', board, 'X');
-    const secondPlayer = new Player('OOZ', board, 'O');
+  // body...
+  const board = new Board();
+  const firstPlayer = new Player('EXES', board, 'X');
+  const secondPlayer = new Player('OOZ', board, 'O');
 
-    let counter = 0;
+  let counter = 0;
 
-    this.start = function(argument) {
-        const options = {
-            childList: true,
-        };
-        const observer = new MutationObserver(() => takeTurns());
-        board.cells.forEach(element => observer.observe(element, options));
-        takeTurns();
+  this.start = function (argument) {
+    const options = {
+      childList: true,
     };
+    const observer = new MutationObserver(() => takeTurns());
+    board.cells.forEach((element) => observer.observe(element, options));
+    takeTurns();
+  };
 
-    function gameReset() {
-        board.reset();
-        return counter = 0;
+  function gameReset() {
+    board.reset();
+    return (counter = 0);
+  }
+
+  function isWinOrDraw() {
+    // check for a win / draw before next turn
+    let decide = false;
+
+    const player = counter % 2 === 0 ? secondPlayer : firstPlayer;
+    if (board.isWinner()) {
+      // congratulations
+      gameUtils.winningMessage(player.name);
+
+      decide = true;
     }
 
-    function isWinOrDraw() {
-        // check for a win / draw before next turn
-        let decide = false;
-
-        const player = counter % 2 === 0 ? secondPlayer : firstPlayer;
-        if (board.isWinner()) {
-            // congratulations
-            gameUtils.winningMessage(player.name);
-
-            decide = true;
-        }
-
-        if (board.isDraw()) {
-            gameUtils.drawMessage(player.name);
-            decide = true;
-        }
-
-        return decide;
+    if (board.isDraw()) {
+      gameUtils.drawMessage(player.name);
+      decide = true;
     }
 
-    function takeTurns() {
-        if (isWinOrDraw()) {
-            return gameReset();
-        }
+    return decide;
+  }
 
-        if (counter % 2 === 0) {
-            firstPlayer.myTurn();
-        } else {
-            secondPlayer.myTurn();
-        }
-        counter += 1;
+  function takeTurns() {
+    if (isWinOrDraw()) {
+      return gameReset();
     }
+
+    if (counter % 2 === 0) {
+      firstPlayer.myTurn();
+    } else {
+      secondPlayer.myTurn();
+    }
+    counter += 1;
+  }
 }
 
 function Player(name, board, signature) {
-    this.name = name;
-    this.signature = signature;
-    // attribute accessor
-    this.getName = () => name;
+  this.name = name;
+  this.signature = signature;
+  // attribute accessor
+  this.getName = () => name;
 
-    this.myTurn = function() {
-        board.cells.forEach(cell => {
-            cell.addEventListener('click', makeMove);
-        });
-    };
-    const makeMove = (event) => {
-        const { target } = event;
-        target.innerText = this.signature;
-        board.cells.forEach(element => element.removeEventListener('click', makeMove));
-    };
+  this.myTurn = function () {
+    board.cells.forEach((cell) => {
+      if (cell.innerHTML===''){
+      cell.addEventListener('click', makeMove);
+      }
+    });
+  };
+  const makeMove = (event) => {
+    const { target } = event;
+    target.innerText = this.signature;
+    board.cells.forEach((element) =>
+      element.removeEventListener('click', makeMove)
+    );
+  };
 }
 
 function Board() {
-    this.cells = Array.from(document.querySelectorAll('div.block'));
+  this.cells = Array.from(document.querySelectorAll('div.block'));
 
+  // check for a winner
+  this.isWinner = function () {
+    let winner = false;
 
-    // check for a winner
-    this.isWinner = function() {
-        let winner = false;
+    const positions = this.cells;
 
-        const positions = this.cells;
+    boardUtils.WINNING_COMBINATIONS.forEach((combination) => {
+      // combinatins are made up of three cells
+      const firstCell = positions[combination[0]].innerText;
+      const secondCell = positions[combination[1]].innerText;
+      const thirdCell = positions[combination[2]].innerText;
 
-        boardUtils.WINNING_COMBINATIONS.forEach((combination) => {
-            // combinatins are made up of three cells
-            const firstCell = positions[combination[0]].innerText;
-            const secondCell = positions[combination[1]].innerText;
-            const thirdCell = positions[combination[2]].innerText;
+      const confirmWin =
+        firstCell !== '' &&
+        secondCell === firstCell &&
+        thirdCell === secondCell;
 
-            const confirmWin = firstCell !== '' && secondCell === firstCell && thirdCell === secondCell;
+      if (confirmWin) {
+        winner = true;
+      }
+    });
 
-            if (confirmWin) {
-                winner = true;
-            }
-        });
+    return winner;
+  };
 
-        return winner;
-    };
+  this.isDraw = function () {
+    return this.cells.every((cell) => ['X', 'O'].includes(cell.innerText));
+  };
 
-    this.isDraw = function() {
-        return this.cells.every(cell => ['X', 'O'].includes(cell.innerText));
-    };
-
-    this.reset = function() {
-        return this.cells.forEach(cell => cell.innerText = '');
-        // body...
-    };
+  this.reset = function () {
+    return this.cells.forEach((cell) => (cell.innerText = ''));
+    // body...
+  };
 }
